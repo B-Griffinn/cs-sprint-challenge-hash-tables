f512aff2a10983ffe15d296ea1ce9090bc1c282a
Ben <bigriffin94@outlook.com>
2020-06-05 10:52:27 -0700

    1 Stretch completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,30 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return route
+    # create cache
+    cache = {}
+    storage = [None] * length
+
+    for i in tickets:
+        if i.source == "NONE":
+            storage[0] = i.destination
+        cache[i.source] = i.destination
+
+    for x in range(1, length):
+        storage[x] = cache[storage[x - 1]]
+
+    return storage
+
+
+""" 
+PLAN
+- create a cache
+- create an arr that grows with length input
+- loop thru our tickets input arr
+- check if our ticket source is none, bc that is our departure ticket aka number 1
+-- if we found none/depart ticket then we assign our storage arr[0] to be our ticket.desination value
+- udpate our cache ticket source to be the tickets destination
+- loop thru range starting at 1 and end at our given length
+- reconstruct our storage arr with the proper order
+"""
