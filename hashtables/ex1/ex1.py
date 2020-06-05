2109e91df5a9929c26831cfaeb54bf4d160d46e6
Ben <bigriffin94@outlook.com>
2020-06-05 10:29:42 -0700

    Merging two packages is working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,19 +1,24 @@
-
-
 def get_indices_of_item_weights(weights, length, limit):
+    # creat a cache
     cache = {}
 
+    # loop through the len of our weights arr so we can extract each index value
     for i in range(0, len(weights)):
-
+        # create a cur variable to track our weight index value
         cur = weights[i]
+        # print(cur)
 
+        # if our weight index value is already in the cache...
         if cur in cache:
-
+            # then we need to store that index in a variable that is to be returned as a tuple
             needed_weight = cache[cur]
-
+            # print(needed_weight)
+            # returning our tuple using the found indecies
             return (i, needed_weight)
 
+        # subract limit from curent value in each cache index and set it to i so that this opperation is conducted each loop
         cache[limit - cur] = i
+    # return none if we can not merge packages
     return None
 
 
