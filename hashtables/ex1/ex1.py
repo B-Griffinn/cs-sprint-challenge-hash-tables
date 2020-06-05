d26b46884fbf0812c7315b23047e150839b73fd2
Ben <bigriffin94@outlook.com>
2020-06-05 09:04:45 -0700

    Passed 1 test

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,41 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# create a cache
+cache = {}
 
-    return None
+
+def get_indices_of_item_weights(weights, length, limit):
+
+    # loop through our weights arr but stop at length
+    for i in range(0, len(weights)):
+        # store each weight as the key
+        # print(weights[i])
+        if weights[i] not in cache:
+            # store each weight index as the value
+            cache[i] = weights[i]
+            # print("CACHE", cache[i])
+
+    # loop thru cache
+    # for j in range(0, len(cache)-1):
+        # print("JJJ", cache[j])
+        # if cache[j]
+    # if key + key == limit
+    # if cache[j] +
+    # return (indexsml, indexbig)
+    # or return none
+    print(cache)
+
+
+l1 = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(l1, 5, 21))
+"""
+PLAN
+- weights == [] list
+- length == len(weights)
+- limit == weight limit we are looking for
+- find 2 items that sum to be `limit`
+- return == (index, index) of two inecies that sum to be `limit`
+- sort return statement so the smaller value is first (smaller, bigger)
+- if a pair does not exist return none
+
+- each weight in weights list can == KEY
+- each weights list index can == VALUE
+"""
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,34 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # create cache
+    cache = {}
 
+    result = []
+
+    # word count approach
+    for i in arrays:
+        for j in i:
+            if j in cache:
+                cache[j] += 1
+            else:
+                cache[j] = 1
+    for x in cache:
+        if cache[x] > 1:
+            result.append(x)
     return result
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(1, 4)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+
+"""
+PLAN
+- use word count approach
+- return number that has a value > 1
+"""
