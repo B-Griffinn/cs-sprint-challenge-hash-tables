697b16b82e6d99cbc42d8db3e45e29a5dd46538f
Ben <bigriffin94@outlook.com>
2020-06-05 09:19:37 -0700

    tests passing OK BUT very slow. need to improve

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,26 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
 
+    result = []
+
+    for i in a:
+        print("I", i)
+        if i not in cache:
+            cache[i] = i
+
+    for j in cache:
+        print('cache[j]', cache[j])
+        if cache[j] < 0 and cache[j] in cache:
+            result.append(abs(cache[j]))
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+
+"""
+use the `abs` method to check if nums are ==
+- create a cache
+- if not 
+"""
